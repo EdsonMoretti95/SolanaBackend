73714b3856ec7494baf50e5407072dc529b85106
Edson Luis Moretti <edsonluismoretti@gmail.com>
2024-06-23 21:16:23 +0100

    changed rpc endpoint
    changed rpc endpoint

--- solanaConnection.js
+++ solanaConnection.js
@@ -1,6 +1,6 @@
 const { Connection } = require('@solana/web3.js');
 
 // Create a connection to the Solana cluster
-const connection = new Connection("https://fancy-daphna-fast-mainnet.helius-rpc.com/", 'confirmed');
+const connection = new Connection("https://evocative-sparkling-leaf.solana-mainnet.quiknode.pro/79735600ada1b45856cc7a3835686c46503cef48/", 'confirmed');
 
 module.exports = connection;
